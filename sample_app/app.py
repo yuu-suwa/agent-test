69add362a4bfe08ab637266d4f7c9498cc2ec877
yuu-suwa <yuu-suwa@7andinm.co.jp>
2026-03-03 07:01:32 +0000

    modify

--- sample_app/app.py
+++ sample_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from auth import login_user
+from auth import user_check
 from utils import calculate_discount
 
 app = Flask(__name__)
@@ -12,16 +12,15 @@ def home():
 def login():
     data = request.get_json(force=True, silent=True)
     if data is None:
-        return jsonify({"message": "Invalid Request"}), 400
+        return jsonify({"message": "Invalid Request. Request body must be valid JSON."}), 400
 
     username = data.get("username")
     password = data.get("password")
 
     if not username or not password:
-        return jsonify({"message": "Invalid Request"}), 400
+        return jsonify({"message": "Invalid Request. Both UserName and Password are required"}), 400
 
-    # パスワード平文比較（NG）
-    if login_user(username, password):
+    if user_check(username, password):
         return jsonify({"message": "login success"})
     else:
         return jsonify({"message": "login failed"}), 401
@@ -31,13 +30,17 @@ def login():
 def price():
     data = request.get_json(force=True, silent=True)
     if data is None:
-        return jsonify({"message": "Invalid Request"}), 400
+        return jsonify({"message": "Invalid Request. Request body must be valid JSON."}), 400
 
     price = data.get("price")
     user_type = data.get("user_type")
 
-    if not price or not isinstance(price, (int, float)) or not user_type:
-        return jsonify({"message": "Invalid Request"}), 400
+    if not price or not user_type:
+        return jsonify({"message": "Invalid Request. Both Price and UserType are required."}), 400
+    if not isinstance(price, (int, float)) or price < 0:
+        return jsonify({"message": "Invalid Request. Price must be positive value."}), 400
+    if user_type not in ("VIP", "NORMAL"):
+        return jsonify({"message": "Invalid Request. Unexpected value passed for UserType."}), 400
 
     final_price = calculate_discount(price, user_type)
 
--- sample_app/auth.py
+++ sample_app/auth.py
@@ -1,9 +1,5 @@
-def login_user(username, password):
-    if user_check( username, password):
-        return True
-    return False
-
 def user_check(username, password):
-    # 別途APIでチェックする想定
-    # 入力されたusername、passwordをusername#passwordの文字列に変換してハッシュ値を送信する
-    return True
+    # 開発中のため仮実装
+    # 本番では別途APIでチェックする想定
+    # 入力されたusername、passwordをusername#passwordの文字列に変換してハッシュ値を送信してチェック結果を参照する
+    return username == 'admin' and password == 'password'
