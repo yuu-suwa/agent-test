cf500e8d73fe6ba0b30f6c3e573935da42b8d0d6
yuu-suwa <yuu-suwa@7andinm.co.jp>
2026-03-04 02:48:08 +0000

    modify

--- sample_app/utils.py
+++ sample_app/utils.py
@@ -1,7 +1,7 @@
 def calculate_discount(price, user_type):
     if user_type == "VIP":
-        return price * 0.7
+        return price * Decimal("0.7")
     elif user_type == "NORMAL":
-        return price * 0.9
+        return price * Decimal("0.9")
     else:
         return price
